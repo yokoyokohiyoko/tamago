f817d1cdfb3e253136779cf4fff585526a50aff9
yokoyokohiyoko <63271567+yokoyokohiyoko@users.noreply.github.com>
2021-05-27 20:03:27 +0900

    Delete pro8 directory

--- pro8/app.js
+++
@@ -1,9 +0,0 @@
-const hamburgerBtn = document.querySelector(".toggle-btn");
-const mainHeader = document.querySelector(".main-header");
-const overlay = document.querySelector(".overlay");
-
-hamburgerBtn.addEventListener("click", function(){
-  hamburgerBtn.classList.toggle("open");
-  overlay.classList.toggle("open");
-  mainHeader.classList.toggle("open");
-});
